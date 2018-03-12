c3c1de360106a40aab4a339e4b30832f1d222c94
Chillar Anand <anand21nanda@gmail.com>
2018-03-12 12:03:06 +0530

    Added weather programme

---
+++ weather.py
@@ -0,0 +1,20 @@
+"""
+Get weather with location, ip address, lat+long
+
+pincode = 560029
+lat_long = '12.91,74.85'
+city = 'Bangalore'
+url = 'https://www.metaweather.com/api/location/search/?query={}'.format(city)
+
+"""
+import json
+from pprint import pprint
+from urllib.request import urlopen
+
+
+url = 'https://www.metaweather.com/api/location/2295420'
+
+response = urlopen(url)
+data = response.read().decode().strip()
+data = json.loads(data)
+pprint(data)
